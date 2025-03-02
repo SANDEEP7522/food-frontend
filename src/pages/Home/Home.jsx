7b93f5e985f578411a9d4d28c97562de3c8ce3ba
SANDEEP7522 <159306588+SANDEEP7522@users.noreply.github.com>
2025-03-02 15:31:35 +0530

    Add Menue

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../../components/Headers/Header'
+import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
 
 function Home() {
   return (
     <div>
       <Header />
+      <ExploreMenu />
     </div>
   )
 }
